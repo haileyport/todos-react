ec1de2c0c37849074c81a61170decbd8a905a008
haileyport <sugar9935@gmail.com>
2022-10-14 21:32:27 +0900

    feat: layout create

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+function Footer() {
+  return (
+    <footer className='footer footer-center p-4 text-base-content'>
+      <div>
+        <p>Copyright © 2022 - All right reserved by Haileyport</p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+function Header() {
+  return (
+    <div className='navbar bg-base-100'>
+      <span className='btn-ghost btn text-xl normal-case'>TODO</span>
+    </div>
+  );
+}
+
+export default Header;
